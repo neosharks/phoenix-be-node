babe4bd6751157625b47efcf83311786130a0de8
Thakur Satyam Singh <satyam.singh@gogroup.co>
2024-02-10 00:09:11 +0530

    removed aws

--- src/controllers/common.controller.ts
+++ src/controllers/common.controller.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from "express";
-import { generateUploadURL } from "../core/uploadS3.core";
 
 class _CommonController {
   async getS3SignedUrl(req: Request, res: Response) {
-    const url = await generateUploadURL();
-    return res.status(200).send({ message: "success", url });
+    return res.status(200).send({ message: "success" });
   }
 }
 
