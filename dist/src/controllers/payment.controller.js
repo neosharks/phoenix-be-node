9459308a735ee06665e3a572c174f1c26837e109
Thakur Satyam <satyam.singh@gogroup.co>
2024-06-20 02:15:13 +0530

    build

--- dist/src/controllers/payment.controller.js
+++ dist/src/controllers/payment.controller.js
@@ -20,12 +20,13 @@ const package_service_1 = require("../services/package.service");
 const config_1 = __importDefault(require("../../config"));
 const axios_1 = __importDefault(require("axios"));
 const payment_service_1 = require("../services/payment.service");
-const payment_validator_1 = require("../validators/payment.validator");
 const package_controller_1 = require("./package.controller");
 cashfree_pg_1.Cashfree.XClientId = config_1.default.payment.cashfree.clientId;
 cashfree_pg_1.Cashfree.XClientSecret = config_1.default.payment.cashfree.clientSecret;
-cashfree_pg_1.Cashfree.XEnvironment = cashfree_pg_1.Cashfree.Environment.PRODUCTION;
-// Cashfree.XEnvironment = Cashfree.Environment.SANDBOX;
+cashfree_pg_1.Cashfree.XEnvironment =
+    config_1.default.payment.cashfree.environment === "PRODUCTION"
+        ? cashfree_pg_1.Cashfree.Environment.PRODUCTION
+        : cashfree_pg_1.Cashfree.Environment.SANDBOX;
 function generateOrderId() {
     const uniqueId = crypto_1.default.randomBytes(16).toString("hex");
     const hash = crypto_1.default.createHash("sha256");
@@ -37,10 +38,8 @@ class _PaymentController {
     order(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const { packageId } = req.body;
-            const validation = payment_validator_1.paymentSchema.validate(packageId);
-            if (validation.error) {
-                return res.status(400).json({ error: validation.error.details[0].message });
-            }
+            if (!packageId)
+                return res.status(api_constant_1.errorCode.GENERIC).json({ message: api_constant_1.errorMessage.MISSING_PARAMS });
             const user = res.locals.user;
             if (!packageId)
                 return res.status(api_constant_1.errorCode.GENERIC).send({ message: api_constant_1.errorMessage.MISSING_PARAMS });
@@ -71,7 +70,7 @@ class _PaymentController {
                 let response;
                 try {
                     response = yield cashfree_pg_1.Cashfree.PGCreateOrder(config_1.default.payment.cashfree.version, request);
-                    console.log(response);
+                    console.log("response ", response);
                 }
                 catch (error) {
                     console.error(error);
@@ -97,10 +96,6 @@ class _PaymentController {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 const { orderId } = req.body;
-                const validation = payment_validator_1.paymentSchema.validate(orderId);
-                if (validation.error) {
-                    return res.status(400).json({ error: validation.error.details[0].message });
-                }
                 if (!orderId)
                     return res.status(api_constant_1.errorCode.GENERIC).send({ message: api_constant_1.errorMessage.MISSING_PARAMS });
                 const url = `${config_1.default.payment.cashfree.url}/orders/${orderId}`;
