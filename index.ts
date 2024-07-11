fa2d7fcdf4f8e7d63504d9d255946f55737ad67d
Thakur Satyam <satyam.singh@gogroup.co>
2024-07-12 01:55:14 +0530

    test commit

--- index.ts
+++ index.ts
@@ -10,3 +10,5 @@ if (res.length < 1) {
     .listen(port, () => logger.info(`SERVER UP AT PORT ${port}`))
     .on("error", (e) => console.log("Error in starting server", e));
 } else logger.info(`SERVER STOPPED DUE TO UNSET ENV VARS: ${res}`);
+
+// temp commit
